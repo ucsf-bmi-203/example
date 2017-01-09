42f77007c1bcd6e18125f262075330b97243c8f4
Tamas Nagy <tamas@tamasnagy.com>
2017-01-08 17:52:48 -0800

    fix relative importing issues

---
+++ example/__main__.py
@@ -0,0 +1,5 @@
+# this file is only called when the package is
+# called from the command line
+from .main import run
+
+run()
--- example/main.py
+++ example/main.py
@@ -1,14 +1,13 @@
 import numpy as np
 import scipy as sp
-import utils
+from .utils import gen_rand_sym_mat
 
 # example useful function
 def calc_deltas(x):
     return x[0:-1] - x[1:]
 
-# main function, only called when this file is
-# executed from the command line
-def main():
+# do things
+def run():
     print("This is the main function")
 
     x = sp.rand(10)
@@ -16,8 +15,4 @@ def main():
     print("Deltas: {0}".format(calc_deltas(x)))
 
     print("\nGenerate random symmetric matrix")
-    print(utils.gen_rand_sym_mat(3))
-
-# if run from the command line
-if __name__ == "__main__":
-    main()
+    print(gen_rand_sym_mat(3))
